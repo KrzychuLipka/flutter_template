dddf79232acd2dce2dae076cc896d5cb48301d4d
Krzysztof Lipka <klpka@n7mobile.com>
2024-06-27 10:18:16 +0200

    README updated

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# flutter_template
+# GeoApp - aplikacja do badañ archeologicznych
+
+## Funkcjonalnoœæ
+### Mapy terenowe
+- Wizualizacja topografii danego obszaru
+### Ró¿ne style mapy
+- Wiek stanowisk
+- Rodzaj znalezionych artefaktów
+### Znaczniki lokalizacji
+- Zaznaczanie miejsc odkryæ archeologicznych w celu œledzenia i dokumentowania znalezisk
+### Opcja zmiany warstwy mapy
+- Granice historycznych osad
+- Stanowiska archeologiczne
+- ród³a wody
+### Wyszukiwarka
+- Badacze mog¹ szybko odnaleŸæ konkretne miejsca na mapie
+### Analiza przestrzenna
+- Obliczanie odleg³oœci miêdzy stanowiskami
+- Tworzenie stref ochronnych
+### Historia i notatki
+- Mo¿lioœæ dodawania: notatek, zdjêæ oraz informacji o odkryciach bezpoœrednio na mapie.
+### Integracja z innymi systemami archeologicznymi
+- Mo¿liowœæ uzyskania pe³niejszych obrazów odkryæ archeologicznych
--- lib/ui/dashboard_widget.dart
+++ lib/ui/dashboard_widget.dart
@@ -20,55 +20,12 @@ class DashboardWidget extends StatelessWidget {
   ) {
     return ArcgisMap(
       apiKey:
-          "AAPK1eb4be74567341b981d400f7f93c4201WSEP1mvip9J-4VdMiNlM1Rmo5ml5eV59N94vMBAnjHj4AwaZZuT2t3diaViORi_3",
-      initialCenter: const LatLng(51.16, 10.45),
-      zoom: 8,
+          "AAPKd7ba0f57475a4ac38a57b649bc5171feUIkt2m3Per1ZdEn-vGRR2XfQRprl-hVuq45ADvgH0A67E-3oVSQg1KdDtfL_rvwU",
+      initialCenter: const LatLng(50.163022, 19.307356),
+      zoom: 15,
       mapStyle: MapStyle.twoD,
-      basemap: BaseMap.arcgisChartedTerritory,
-      // onMapCreated: (controller) async {
-      //   const pinLayerId = 'pins';
-      //   await controller.addGraphic(
-      //     layerId: pinLayerId,
-      //     graphic: PointGraphic(
-      //       latitude: 51.091062,
-      //       longitude: 6.880812,
-      //       attributes: Attributes({
-      //         'id': 'phntm',
-      //         'name': 'PHNTM GmbH',
-      //         'family': 'Office',
-      //       }),
-      //       symbol: const PictureMarkerSymbol(
-      //         webUri:
-      //             "https://github.com/fluttercommunity/arcgis_map/assets/1096485/94178dba-5bb8-4f1e-a160-31bfe4c93d17",
-      //         mobileUri:
-      //             "https://github.com/fluttercommunity/arcgis_map/assets/1096485/94178dba-5bb8-4f1e-a160-31bfe4c93d17",
-      //         width: 314 / 2,
-      //         height: 120 / 2,
-      //       ),
-      //     ),
-      //   );
-      //
-      //   await controller.addGraphic(
-      //     layerId: pinLayerId,
-      //     graphic: PointGraphic(
-      //       latitude: 48.1234963,
-      //       longitude: 11.5910182,
-      //       attributes: Attributes({
-      //         'id': 'tapped',
-      //         'name': 'Tapped UG',
-      //         'family': 'Office',
-      //       }),
-      //       symbol: const PictureMarkerSymbol(
-      //         webUri:
-      //             "https://github.com/fluttercommunity/arcgis_map/assets/1096485/c84c524c-78b7-46e5-9bf1-a3a91853b2cf",
-      //         mobileUri:
-      //             "https://github.com/fluttercommunity/arcgis_map/assets/1096485/c84c524c-78b7-46e5-9bf1-a3a91853b2cf",
-      //         width: 312 / 2,
-      //         height: 111 / 2,
-      //       ),
-      //     ),
-      //   );
-      // },
+      basemap: BaseMap.arcgisTopographic,
+      onMapCreated: (controller) async {},
     );
   }
 }
